0e2b34681d38abd821fca15281a4f33744d76417
Tao Bojlén <tao.bojlen@intruder.io>
2021-12-26 22:59:04 +0000

    fix plausible links round 2

--- layouts/default.vue
+++ layouts/default.vue
@@ -54,9 +54,10 @@ onMounted(() => {
   if (!process.client) return
 
   const script = document.createElement("script")
-  script.src = "/p/pjs/script.outbound-links.js"
+  script.src =
+    "https://plaus-prox.btao.workers.dev/pjs/script.outbound-links.js"
+  script["data-api"] = "https://plaus-prox.btao.workers.dev/api/event"
   script["data-domain"] = "shroud.email"
-  script["data-api"] = "/p/api/event"
   script.defer = true
   document.head.appendChild(script)
 })
