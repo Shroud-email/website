7bbedd124eb6c09c5b2493217acb009eacd47ab6
Tao Bojlén <2803708-tao_oat@users.noreply.gitlab.com>
2022-05-30 22:04:03 +0100

    fix build

--- src/components/organisms/HowItWorksSection.vue
+++ src/components/organisms/HowItWorksSection.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-import { ArrowCircleRightIcon, EyeOffIcon, TrashIcon } from '@heroicons/vue/outline'
+import { ArrowCircleRightIcon, EyeOffIcon, TrashIcon } from '@heroicons/vue/outline/esm/index.js'
 
 import FadeIn from "../atoms/FadeIn.vue"
 
